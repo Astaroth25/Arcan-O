b9f01baae24a9ecd2c08fd32928adb3253c3a186
Astaroth25 <astaroth25.dev@gmail.com>
2025-08-22 12:37:03 -0500

    Creación del componente de antorcha de pared.

---
+++ src/app/Shared/Lighting/wall-torch/wall-torch.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'wall-torch',
+  imports: [],
+  templateUrl: './wall-torch.html',
+  styleUrl: './wall-torch.scss'
+})
+export class WallTorch {
+}
