88bb47877aec97d814926917f53ed4c35df8421a
Astaroth25 <astaroth25.dev@gmail.com>
2025-08-31 19:16:42 -0500

    Actualización para que el componente controle su estado de encendido y apagado

--- src/app/Shared/Lighting/wall-torch/wall-torch.ts
+++ src/app/Shared/Lighting/wall-torch/wall-torch.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { LightingStateService } from '../../Services/lighting-state';
+import { AsyncPipe } from '@angular/common';
 
 @Component({
   selector: 'wall-torch',
-  imports: [],
+  imports: [AsyncPipe],
   templateUrl: './wall-torch.html',
   styleUrl: './wall-torch.scss'
 })
 export class WallTorch {
+  private lightingStateService = inject(LightingStateService);
+  protected lit$ = this.lightingStateService.lit$;
+
+  changeLit(): void {
+    this.lightingStateService.changeLit();
+  }
 }
