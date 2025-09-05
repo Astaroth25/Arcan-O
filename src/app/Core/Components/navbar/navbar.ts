7176e1b429ecb62615d7e41d983d3f9e343cd36d
Astaroth25 <astaroth25.dev@gmail.com>
2025-09-05 14:14:47 -0500

    Creación del componente navbar

---
+++ src/app/Core/Components/navbar/navbar.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { Basic } from "../../../Shared/Components/Buttons/basic/basic";
+
+@Component({
+  selector: 'navbar',
+  imports: [Basic],
+  templateUrl: './navbar.html',
+  styleUrl: './navbar.scss'
+})
+export class Navbar {
+
+}
